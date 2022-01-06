6b919f724b7950b5376f3706b03e93c63484e77b
Nagoor-K <p.nagoor@axelor.com>
2022-01-06 11:58:01 +0530

    Addng two new files

---
+++ prntodd.sh
@@ -0,0 +1,9 @@
+echo "enter a value to check"
+read k
+if [ `expr $k % 2` -eq 0 ]
+then
+	echo "this is even number $k"
+
+else
+	echo "this is odd number $k"
+fi
---
+++ sumofn.sh
@@ -0,0 +1,10 @@
+echo "enter n value"
+read n
+k=1
+sum=0
+while [ $k -le $n ]
+do
+	sum=`expr $sum + $k `
+	k=`expr $k + 1 ` 
+done
+echo "sum of $n natural numbers is $sum"
